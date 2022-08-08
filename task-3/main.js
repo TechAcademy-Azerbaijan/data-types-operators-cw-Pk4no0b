ff4a6e19910455e13e128a474082846f4cf3319e
Selim Mammadov <mamedovfhn@gmail.com>
2022-08-08 19:34:12 +0400

    Homework task 2-3

--- task-3/main.js
+++ task-3/main.js
@@ -3,7 +3,14 @@ prompt.start();
 
 
 prompt.get('input', function (err, result) {
+
+var input = result.input;
+var numbers = input.split(',')
+var firstNumber = parseInt(numbers[0]);
+var secondNumber = parseInt(numbers[1]);
+var resultNumber = firstNumber**secondNumber
+console.log(resultNumber)
   
-    // Write code here
-  
+
+
 });
