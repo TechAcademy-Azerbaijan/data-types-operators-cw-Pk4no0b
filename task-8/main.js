4cb0b8c5780076e4ca7b9a6c7d7a6cc77ec0adda
Selim Mammadov <mamedovfhn@gmail.com>
2022-08-09 11:20:04 +0400

    Homework task-8

--- task-8/main.js
+++ task-8/main.js
@@ -4,6 +4,10 @@ prompt.start();
 
 prompt.get('input', function (err, result) {
   
-    // Write code here
+let a = parseInt(result.input); //123
+let c = (a%10) //3
+let b =parseInt(a/10); //12
+let cvb = c*100 + b*1 //312
+  console.log (cvb);
   
 });
