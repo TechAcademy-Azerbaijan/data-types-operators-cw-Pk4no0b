51a756b755c0d2cbd4567f579048b874af7e6526
Selim Mammadov <mamedovfhn@gmail.com>
2022-08-09 11:16:59 +0400

    Homework task-7

--- task-7/main.js
+++ task-7/main.js
@@ -4,6 +4,12 @@ prompt.start();
 
 prompt.get('input', function (err, result) {
   
-    // Write code here
-  
+let a = parseInt(result.input) //3456
+let b = parseInt(a%10) //6
+let c = parseInt((a%100)/10) //5
+let d = parseInt((a%1000)/100) //4
+let k = parseInt(a/1000) //3
+let cvb = ((b*1000)+(c*100)+(d*10)+(k*1))
+console.log (cvb)
+
 });
