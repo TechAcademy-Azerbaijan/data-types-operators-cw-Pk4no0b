5869a1834f76b3fe0cb4eb202d91aa0842e68a49
Selim Mammadov <mamedovfhn@gmail.com>
2022-08-15 20:33:32 +0400

    task-1

--- task-1/main.js
+++ task-1/main.js
@@ -4,6 +4,13 @@ prompt.start();
 
 prompt.get('input', function (err, result) {
   
-    // Write code here
+var input = result.input;
+var numbers = input.split(' ');
+var x =parseInt(numbers[0]);
+var n =parseInt(numbers[1]);
+var m =parseInt(numbers[2]);
+var d =(x**n)%m
+
+console.log(d)
   
 });
